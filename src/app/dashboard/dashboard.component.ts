192cef84e58473409de5cc2b35916dbee9858f59
DESKTOP-O219LQR\PalmNatthasit <znatthasit.s@pttdigital.com>
2022-04-21 10:14:27 +0700

    complete spinner loading

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  timeOut: boolean = true;
 
+  constructor() {}
+    
+  ngOnInit(): void {
+    this.timeOut = true;
+    setTimeout(() => {
+      this.timeOut = false;
+    },1500);
+  }
 }
